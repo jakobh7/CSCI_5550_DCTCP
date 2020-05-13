824d4e193b2603e1106906e1daedbbc1f3d7fa51
Mohammad Hadi <42990664+mhab23@users.noreply.github.com>
2020-05-13 17:58:25 -0500

    Add files via upload

--- dctop.py
+++ dctop.py
@@ -4,19 +4,38 @@ from mininet.util import irange
 class MyTOPO( Topo ):
 	"Datacenter Topology"
 	
-	def build( self ):
-		self.racks = []
-		rootSwitch = self.addSwitch( 's1' )
-		for i in irange( 1, 4 ):
-			rack = self.buildRack( i )
-			self.racks.append( rack )
-			for switch in rack:
-				self.addLink( rootSwitch, switch )
-	def buildRack( self, loc ):
-		dpid = ( loc * 16 ) + 1
-		switch = self.addSwitch( 's1r%s' % loc, dpid = '%x' % dpid )
-		for n in irange( 1, 4 ):
-			host = self.addHost( 'h%sr%s' % ( n, loc ) )
-			self.addLink( switch, host )
-		return [switch]
+	def __init__(self):
+
+		Topo.__init__(self)
+
+		#switches
+		
+		s1 = self.addSwitch('s1')
+		s11 = self.addSwitch('s11')
+		s12 = self.addSwitch('s12')
+		s111 = self.addSwitch('s111')
+		s112 = self.addSwitch('s112')
+		s121 = self.addSwitch('s121')
+		s122 = self.addSwitch('s122')
+
+		#hosts
+		h1 = self.addHost('h1')
+		h2 = self.addHost('h2')
+		h3 = self.addHost('h3')
+		h4 = self.addHost('h4')
+		
+
+		#links
+		self.addLink( s1 , s11 )
+		self.addLink( s1 , s12 )
+		self.addLink( s11 , s111 )
+		self.addLink( s11 , s112 )
+		self.addLink( s12 , s121 )
+		self.addLink( s12 , s122 )
+		
+		self.addLink( s111 , h1 )
+		self.addLink( s112 , h2 )
+		self.addLink( s121 , h3 )
+		self.addLink( s122 , h4 )
+
 topos = { 'datacenter' : MyTOPO }
